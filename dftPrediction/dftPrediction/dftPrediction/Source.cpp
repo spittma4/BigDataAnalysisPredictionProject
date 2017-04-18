4fdf343fa0f210cafb21b46160fdf9ef00aa68c8
spittma4 <spittma4@kent.edu>
2017-04-18 14:44:21 -0700

    Add files via upload
    Initial commit/outline of DFT method

---
+++ dftPrediction/dftPrediction/dftPrediction/Source.cpp
@@ -0,0 +1,44 @@
+/*
+Big Data Analytics, Spring 2017
+Data Predicition, Discrete Fourier Transformation method prediction algorithm
+----------
+Written by:
+---
+Samuel Pittman
+Adam Duryea
+Michael Caprile
+Shane Favours
+*/
+
+/*
+PSEUDO CODE:
+Loop to process an element at a time, iterate until no more new cases{
+Array of ADT that will hold the surveyed value and the number of times it has appeared (to get the frequency)
+
+Check if value is in array{
+If in array -> increment that node
+If NOT in array -> add node with that value and initialize to have a frequency of 1
+}
+
+Order array largest to smallest
+}
+
+The prediction will be the value with the most frequency, at the front of the array
+*/
+
+#include <iostream>
+
+using std::cout; using std::cin; using std::endl;
+
+//Test Dataset
+int testData[20] = { 5, 7, 9, 50, 5, 8, 2, 30, 7, 1, 11, 15, 7, 6, 16, 23, 15, 16, 8, 5 };
+
+//Methods 
+
+
+
+//Main function
+int main() {
+
+	return 0;
+}
